da2d004fa3fb041a2a621f7022996cc8514ca438
Ardhan12 <arief.ramadhan43@gmail.com>
2022-12-20 10:37:41 +0700

    Initial Commit

---
+++ FoodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodApp
+//
+//  Created by Arief Ramadhan on 20/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
